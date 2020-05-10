ece9c95be21335d742655f0a16e38104dca17ad7
xxsacxx <https://gitlab.onionlife.in/>
2020-05-10 18:53:48 +0530

    Datastream and table

---
+++ flink/tableapi-python/WordCount.py
@@ -0,0 +1,33 @@
+from pyflink.dataset import ExecutionEnvironment
+from pyflink.table import TableConfig, DataTypes, BatchTableEnvironment
+from pyflink.table.descriptors import Schema, OldCsv, FileSystem
+
+exec_env = ExecutionEnvironment.get_execution_environment()
+exec_env.set_parallelism(1)
+t_config = TableConfig()
+## creating BatchTableEvironment for batch applications
+t_env = BatchTableEnvironment.create(exec_env, t_config)
+## declare source and sink tables.
+t_env.connect(FileSystem().path('input')) \
+    .with_format(OldCsv()
+                 .field('word', DataTypes.STRING())) \
+    .with_schema(Schema()
+                 .field('word', DataTypes.STRING())) \
+    .create_temporary_table('mySource')
+
+t_env.connect(FileSystem().path('output')) \
+    .with_format(OldCsv()
+                 .field_delimiter('\t')
+                 .field('word', DataTypes.STRING())
+                 .field('count', DataTypes.BIGINT())) \
+    .with_schema(Schema()
+                 .field('word', DataTypes.STRING())
+                 .field('count', DataTypes.BIGINT())) \
+    .create_temporary_table('mySink')
+## job
+t_env.from_path('mySource') \
+    .group_by('word') \
+    .select('word, count(1)') \
+    .insert_into('mySink')
+
+t_env.execute("tutorial_job")
